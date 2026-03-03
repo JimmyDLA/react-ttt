01ec239cf88184874c032fb1c8166c4d17e5fe5e
Jimmy DLA <jimmydelosangeles@gmail.com>
2026-03-03 14:46:31 -0400

    added beautiful style

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import '../App.css'
 
-export const Box = ({symbol, onPress, isHightLight, winner }) => {
+export const Box = ({ symbol, onPress, isHightLight, winner }) => {
   const isDraw = winner === 'NONE';
-  
+  const symbolClass = symbol === 'X' ? 'x' : symbol === 'O' ? 'o' : '';
+
   return (
-    <button className={`box ${isDraw ? 'draw': ''}`} onClick={onPress} id={isHightLight ? "high" : "low"}>
+    <button className={`box ${isDraw ? 'draw' : ''} ${symbolClass}`} onClick={onPress} id={isHightLight ? "high" : "low"}>
       <h1>{symbol}</h1>
     </button>
   )
